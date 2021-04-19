d620f661952149631ba7787cd2c511b6f85a6571
Artem <aopoltorzhicky@gmail.com>
2021-04-19 19:29:18 +0300

    Database connection to internal. go mod tidy.

--- cmd/mempool/models/database.go
+++ cmd/mempool/models/database.go
@@ -1,49 +1,14 @@
 package models
 
 import (
-	"log"
-	"os"
-	"time"
-
 	"github.com/dipdup-net/mempool/internal/config"
 	"github.com/dipdup-net/mempool/internal/state"
-	"github.com/pkg/errors"
-	"gorm.io/driver/clickhouse"
-	"gorm.io/driver/mysql"
-	"gorm.io/driver/postgres"
-	"gorm.io/driver/sqlite"
-	"gorm.io/driver/sqlserver"
 	"gorm.io/gorm"
-	"gorm.io/gorm/logger"
 )
 
 // OpenDatabaseConnection -
 func OpenDatabaseConnection(cfg config.Database, kinds ...string) (*gorm.DB, error) {
-	var dialector gorm.Dialector
-	switch cfg.Kind {
-	case DBKindSqlite:
-		dialector = sqlite.Open(cfg.Path)
-	case DBKindPostgres:
-		dialector = postgres.Open(cfg.Path)
-	case DBKindMysql:
-		dialector = mysql.Open(cfg.Path)
-	case DBKindClickHouse:
-		dialector = clickhouse.Open(cfg.Path)
-	case DBKindSqlServer:
-		dialector = sqlserver.Open(cfg.Path)
-	default:
-		return nil, errors.Errorf("Unsupported database %s", cfg.Kind)
-	}
-
-	db, err := gorm.Open(dialector, &gorm.Config{
-		Logger: logger.New(
-			log.New(os.Stdout, "\r\n", log.LstdFlags),
-			logger.Config{
-				SlowThreshold:             time.Second,
-				IgnoreRecordNotFoundError: true,
-			},
-		),
-	})
+	db, err := state.OpenConnection(cfg.Kind, cfg.Path)
 	if err != nil {
 		return nil, err
 	}
@@ -51,5 +16,14 @@ func OpenDatabaseConnection(cfg config.Database, kinds ...string) (*gorm.DB, err
 	data := GetModelsBy(kinds...)
 	data = append(data, &state.State{})
 
-	return db, db.AutoMigrate(data...)
+	if err := db.AutoMigrate(data...); err != nil {
+		sql, err := db.DB()
+		if err == nil {
+			if err := sql.Close(); err != nil {
+				return nil, err
+			}
+		}
+		return nil, err
+	}
+	return db, nil
 }
--- go.mod
+++ go.mod
@@ -3,25 +3,23 @@ module github.com/dipdup-net/mempool
 go 1.15
 
 require (
-	github.com/dipdup-net/tzktevents v0.0.0-20210414153332-d3bd188571a7
-	github.com/ebellocchia/go-base58 v0.1.0
+	github.com/dipdup-net/tzktevents v0.0.0-20210416132536-72a7fbbb8c3d
 	github.com/json-iterator/go v1.1.10
 	github.com/karlseguin/ccache v2.0.3+incompatible
 	github.com/karlseguin/expect v1.0.8 // indirect
-	github.com/kr/pretty v0.2.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/sirupsen/logrus v1.8.1
-	github.com/stretchr/testify v1.6.1
-	golang.org/x/sys v0.0.0-20210414055047-fe65e336abe0 // indirect
-	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
+	github.com/stretchr/testify v1.7.0
+	golang.org/x/sys v0.0.0-20210415045647-66c3f260301c // indirect
+	gopkg.in/check.v1 v1.0.0-20201130134442-10cb98267c6c // indirect
 	gopkg.in/yaml.v2 v2.4.0
-	gopkg.in/yaml.v3 v3.0.0-20200605160147-a5ece683394c // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	gorm.io/driver/clickhouse v0.1.0
 	gorm.io/driver/mysql v1.0.5
 	gorm.io/driver/postgres v1.0.8
 	gorm.io/driver/sqlite v1.1.4
 	gorm.io/driver/sqlserver v1.0.7
-	gorm.io/gorm v1.21.7
+	gorm.io/gorm v1.21.8
 )
