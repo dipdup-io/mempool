cb0bfc8f883d631a57b17d69731988a4133eecbf
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-06-28 20:33:13 +0300

    Added doc-comments for Reveal.

--- cmd/mempool/models/reveal.go
+++ cmd/mempool/models/reveal.go
@@ -3,12 +3,12 @@ package models
 // Reveal -
 type Reveal struct {
 	//nolint
-	tableName struct{} `pg:"reveals"`
+	tableName struct{} `pg:"reveals" comment:"reveal operation - is used to reveal the public key associated with an account."`
 	MempoolOperation
-	Source       string `json:"source" index:"reveal_source_idx"`
-	Fee          int64  `json:"fee,string"`
-	Counter      int64  `json:"counter,string" pg:",pk" `
-	GasLimit     int64  `json:"gas_limit,string"`
-	StorageLimit int64  `json:"storage_limit,string"`
-	PublicKey    string `json:"public_key"`
+	Source       string `json:"source" index:"reveal_source_idx" comment:"Address of the account who has sent the operation."`           // DISCUSS
+	Fee          int64  `json:"fee,string" comment:"Fee to the baker, produced block, in which the operation was included (micro tez)."` // DISCUSS
+	Counter      int64  `json:"counter,string" pg:",pk" comment:"An account nonce which is used to prevent operation replay."`
+	GasLimit     int64  `json:"gas_limit,string" comment:"A cap on the amount of gas a given operation can consume."`
+	StorageLimit int64  `json:"storage_limit,string" comment:"A cap on the amount of storage a given operation can consume."`
+	PublicKey    string `json:"public_key" comment:"Public key of source address."` // DISCUSS
 }
