fe2a957be551050303c81bae6b385a2f2af5a687
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-06-28 13:54:00 +0300

    Added doc-comments for Ballot.

--- cmd/mempool/models/activate_account.go
+++ cmd/mempool/models/activate_account.go
@@ -6,5 +6,5 @@ type ActivateAccount struct {
 	tableName struct{} `pg:"activate_account" comment:"Type of the operation, activation - is used to activate accounts that were recommended allocations of tezos tokens for donations to the Tezos Foundation’s fundraiser."`
 	MempoolOperation
 	Pkh    string `json:"pkh" comment:"Public key hash (Ed25519). Address to activate."`
-	Secret string `json:"secret" comment:"The secret key associated with the key, if available."`
+	Secret string `json:"secret" comment:"The secret key associated with the key, if available. /^([a-zA-Z0-9][a-zA-Z0-9])*$/"`
 }
