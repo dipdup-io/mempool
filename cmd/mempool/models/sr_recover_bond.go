e52452d57865f4c27341badecf81892539450602
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-06-28 20:55:13 +0300

    Added doc-comments for SmartRollupRecoverBond.

--- cmd/mempool/models/sr_recover_bond.go
+++ cmd/mempool/models/sr_recover_bond.go
@@ -6,12 +6,12 @@ type SmartRollupRecoverBond struct {
 	tableName struct{} `pg:"sr_recover_bond"`
 
 	MempoolOperation
-	Fee          int64  `json:"fee,string"`
-	Counter      int64  `pg:",pk" json:"counter,string"`
-	GasLimit     int64  `json:"gas_limit,string"`
-	StorageLimit int64  `json:"storage_limit,string"`
-	Source       string `json:"source,omitempty" index:"sr_recover_bond_source_idx"`
-	Rollup       string `json:"rollup,omitempty" index:"sr_recover_bond_rollup_idx"`
+	Fee          int64  `json:"fee,string" comment:"Fee to the baker, produced block, in which the operation was included (micro tez)."` // DISCUSS
+	Counter      int64  `pg:",pk" json:"counter,string" comment:"An account nonce which is used to prevent operation replay."`
+	GasLimit     int64  `json:"gas_limit,string" comment:"A cap on the amount of gas a given operation can consume."`
+	StorageLimit int64  `json:"storage_limit,string" comment:"A cap on the amount of storage a given operation can consume."`
+	Source       string `json:"source,omitempty" index:"sr_recover_bond_source_idx" comment:"Address of the account who has sent the operation."` // DISCUSS
+	Rollup       string `json:"rollup,omitempty" index:"sr_recover_bond_rollup_idx" comment:"Smart rollup to which the operation was sent."`
 }
 
 // SetMempoolOperation -
