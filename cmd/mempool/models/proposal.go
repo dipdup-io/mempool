505425ab331314f00f0068ac89fad29342a1b11d
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-06-28 15:40:57 +0300

    Added doc-comments for Proposal.

--- cmd/mempool/models/proposal.go
+++ cmd/mempool/models/proposal.go
@@ -3,8 +3,8 @@ package models
 // Proposal -
 type Proposal struct {
 	//nolint
-	tableName struct{} `pg:"proposals"`
+	tableName struct{} `pg:"proposals" comment:"proposal operation - is used by bakers (delegates) to submit and/or upvote proposals to amend the protocol."`
 	MempoolOperation
-	Period    int64  `json:"period"`
-	Proposals string `json:"proposal" pg:"proposals"`
+	Period    int64  `json:"period" comment:"Voting period index (starting from zero) for which the proposal was submitted (upvoted)."` // DISCUSS
+	Proposals string `json:"proposal" pg:"proposals" comment:"Information about the submitted (upvoted) proposal."`                     // DISCUSS
 }
