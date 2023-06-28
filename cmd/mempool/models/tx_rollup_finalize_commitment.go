924c45c43ca6ed361aada5e95e4ed2154d8a23e7
Lavysh Alexander <LavyshAlexander@gmail.com>
2023-06-28 21:05:58 +0300

    Added doc-comments for TxRollupFinalizeCommitment.

--- cmd/mempool/models/tx_rollup_finalize_commitment.go
+++ cmd/mempool/models/tx_rollup_finalize_commitment.go
@@ -6,12 +6,12 @@ type TxRollupFinalizeCommitment struct {
 	tableName struct{} `pg:"tx_rollup_finalize_commitment"`
 
 	MempoolOperation
-	Fee          int64  `json:"fee,string"`
-	Counter      int64  `pg:",pk" json:"counter,string"`
-	GasLimit     int64  `json:"gas_limit,string"`
-	StorageLimit int64  `json:"storage_limit,string"`
-	Source       string `json:"source,omitempty" index:"tx_rollup_finalize_commitment_source_idx"`
-	Rollup       string `json:"rollup,omitempty" index:"tx_rollup_finalize_commitment_rollup_idx"`
+	Fee          int64  `json:"fee,string" comment:"Fee to the baker, produced block, in which the operation was included (micro tez)."` // DISCUSS
+	Counter      int64  `pg:",pk" json:"counter,string" comment:"An account nonce which is used to prevent operation replay."`
+	GasLimit     int64  `json:"gas_limit,string" comment:"A cap on the amount of gas a given operation can consume."`
+	StorageLimit int64  `json:"storage_limit,string" comment:"A cap on the amount of storage a given operation can consume."`
+	Source       string `json:"source,omitempty" index:"tx_rollup_finalize_commitment_source_idx" comment:"Address of the account who has sent the operation."` // DISCUSS
+	Rollup       string `json:"rollup,omitempty" index:"tx_rollup_finalize_commitment_rollup_idx" comment:"Address of the rollup to which the operation was sent."`
 }
 
 // SetMempoolOperation -
